fd457103241c31778e17cd37083bdbdd58a49026
NadjaUst <example@gmail.com>
2023-10-19 14:21:56 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Tabs
+
+    const tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items")
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+    // Timer
+
+    const deadline = '2023-10-21';
+
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(total / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((total / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((total / 1000 / 60) % 60),
+              seconds = Math.floor((total / 1000) % 60);
+
+               
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds 
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds')
+         
+        const timeInterval = setInterval(updateClock, 1000)
+
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <=0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+})
